b284fe3ad4e6d5c2e03fbcbd28c44b9f019ed177
s00singla <s00singla@gmail.com>
2021-08-21 10:47:38 +0530

    Update Codefile.R

--- Corrplot/Codefile.R
+++ Corrplot/Codefile.R
@@ -1,9 +1,9 @@
 ## tHE CODE GOES LIKE THIS
 library(ggplot2)
 library(corrplot)
-CORRPLOT <- read_excel("C:/Users/Saurav Singla/Downloads/CORRPLOT.xlsx")
-corr11=(CORRPLOT[,-1])
-colnames(corr11)
+CORRPLOT_Data <- read_excel("C:/Users/Saurav Singla/Downloads/CORRPLOT.xlsx")
+corr11=(CORRPLOT_Data[,-1])
+# convert the names in expession form assigning colnames(corr11) as below
 ddmd <- c(expression(Y,S^(1),S^(2), S^(3),
                      S[(6)], NP^(1), NP^(2), NP^(3), NP^(4),W[i]^2,
                      sigma[i]^2,s^(2)*d[i],b[i], CV[i],theta[(i)], theta[i],
